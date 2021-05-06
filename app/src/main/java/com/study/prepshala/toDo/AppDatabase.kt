9e236ad876495ba1c547c1d57ed0ba6a823e472a
siddharth0600 <siddharthtripathi00@gmail.com>
2021-05-06 23:08:10 +0530

    Toolbar and home layout done video3,4

--- app/src/main/java/com/study/prepshala/toDo/AppDatabase.kt
+++ app/src/main/java/com/study/prepshala/toDo/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.study.prepshala.toDo
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [TodoModel::class], version = -1)
+@Database(entities = [TodoModel::class], version = 1)
 abstract class AppDatabase : RoomDatabase(){
     abstract fun todoDao(): TodoDao
 }
--- app/src/main/java/com/study/prepshala/toDo/ToDoActivity.kt
+++ app/src/main/java/com/study/prepshala/toDo/ToDoActivity.kt
@@ -1,12 +1,36 @@
 package com.study.prepshala.toDo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import com.study.prepshala.R
+import kotlinx.android.synthetic.main.activity_to_do.*
 
 class ToDoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_to_do)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.history->{
+                startActivity(Intent(this,HistoryActivity::class.java))
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    fun openNewTask(view: View) {
+        startActivity(Intent(this, TaskActivity::class.java))
     }
 }
